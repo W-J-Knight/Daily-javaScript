a087d43a38a3edc46eff8e6e0898e2a93498e61c
William Knight <William.Jeffery.Knight@protonmail.com>
2022-07-05 09:15:31 -0400

    a way to up a varable

--- class-js/class.js
+++ class-js/class.js
@@ -14,15 +14,21 @@ class User{
     constructor(name, email){
         this.name = name;
         this.email = email;
+        this.score = 0;
     }
     login = () => `${this.email} has logged in`;
     logout = () => `${this.email} has logged out`;
+    updateScore(){
+        this.score++;
+        return `${this.email} Score is now ${this.score}`
+    }
 }
 userOne.login();
 userOne.logout();
 const user1 = new User("Bill", "bill@thehill.com")
 console.log(user1.logout())
-let prop = 'name'
-console.log(userOne[prop])
-prop = 'email'
-console.log(userOne[prop])
+// let prop = 'name'
+// console.log(userOne[prop])
+// prop = 'email'
+// console.log(userOne[prop])
+console.log(user1.updateScore())
