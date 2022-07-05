36aa7767908b66b992a8e753536793bb966fdef5
William Knight <William.Jeffery.Knight@protonmail.com>
2022-07-05 08:47:28 -0400

    run some test

--- class-js/class.js
+++ class-js/class.js
@@ -15,9 +15,10 @@ class User{
         this.name = name;
         this.email = email;
     }
-    login = () => `${this.email} has logged in`
+    login = () => `${this.email} has logged in`;
+    logout = () => `${this.email} has logged out`;
 }
 userOne.login();
 userOne.logout();
 const user1 = new User("Bill", "bill@thehill.com")
-console.log(user1.login())
+console.log(user1.logout())
