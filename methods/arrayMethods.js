1297169b9527680a8b490b67031a5770edf6e191
William <William.Jeffery.Knight@protonmail.com>
2022-07-29 06:30:07 -0400

    today pratctice

---
+++ methods/arrayMethods.js
@@ -0,0 +1,45 @@
+const things = [
+    {
+        id: 0,
+        title: 'whiskers on kittens',
+        favorite: true,
+        points: 97
+    },
+    {
+        id: 1,
+        title: 'raindrops on roses',
+        favorite: true,
+        points: 77
+    },
+    {
+        id: 2,
+        title: 'brown paper packages tied up with string',
+        favorite: true,
+        points: 87
+    },
+    {
+        id: 3,
+        title: 'dog bite',
+        favorite: false,
+        featured: 'true',
+        points: 12
+    },
+    {
+        id: 4,
+        title: 'bee sting',
+        favorite: false,
+        points: 6
+    }
+];
+ const cl = (x) => console.log(x)
+const myFavoriteThings = things.filter((thing) => thing.favorite);
+cl(myFavoriteThings)
+
+const  thingsWithOverFiftyPoints = things.filter((thing) => thing.points > 50)
+cl(thingsWithOverFiftyPoints)
+
+const selectThingByID = (id) => {
+    return things.find((thing) => thing.id === id)
+}
+
+cl(selectThingByID(2))
